6c258fe5df491959e3944c430a3dce6b11ed1114
perness <khanim14@student.westerdals.no>
2018-10-12 07:20:30 +0200

    message

--- src/main/kotlin/com/spring/bet_calculator/api/OpponentRestApi.kt
+++ src/main/kotlin/com/spring/bet_calculator/api/OpponentRestApi.kt
@@ -4,13 +4,12 @@ import com.spring.bet_calculator.Constants
 import com.spring.bet_calculator.repositories.OpponentRepository
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @Api(
         value = "/opponents",
@@ -31,8 +30,21 @@ class OpponentRestApi
 
     @ApiOperation("Add win to ratio")
     @PatchMapping(path = ["/{id}/win"], consumes = [(MediaType.TEXT_PLAIN_VALUE)])
-    fun addWin()
-    {}
+    fun addWin(
+            @ApiParam("The id of the opponent")
+            @PathVariable("id")
+            id: Long) : ResponseEntity<Void>
+    {
+        val entity = crud.findById(id)
+        val ratio = entity.get().ratio + 1
+
+        if (crud.updateRatio(id, ratio))
+        {
+            return ResponseEntity.status(204).build()
+        }
+
+        return ResponseEntity.status(400).build()
+    }
 
 }
 
